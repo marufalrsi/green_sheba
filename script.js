aef8dcc745f9b04ff70683a44dcdfabfd4296b38
marufalrsi <marufallrushafi11@gmail.com>
2026-03-14 08:51:59 +0600

    2nd update

---
+++ script.js
@@ -0,0 +1,69 @@
+const disableactive=()=>{
+    const btns=document.querySelectorAll(".btns")
+    btns.forEach(x=>x.classList.remove("active"))
+    
+}
+const addtocart=(x,y)=>{
+    let clicked=0
+    document.getElementById("cartinfo").innerHTML+=`<div class="bg-[#b3f3cc60] py-3 px-5 flex justify-between my-2">
+    <div>
+        <h1>${x}</h1>
+        <h2>৳${y} <i class="fa-solid fa-pen"></i>x <span>1</span></h2>
+   </div>
+    <div><h1 class="text-2xl ">x</h1></div>
+   </div>`
+}
+const displayplantinfo=(y)=>{
+    document.getElementById("trees").innerHTML=``
+//    console.log(y)
+   let treeinfo=``
+   y.forEach(z=>{
+    treeinfo+=`<div class="my-3 p-3 bg-white rounded-sm sm:my-0">
+            <div>
+                <img class="h-30 w-full" src=${z.image} alt="">
+            </div>
+            <div class="h-30">
+            <h6 class="font-semibold text-[14px] py-1"></h6>
+            <h5 class="font-normal text-[12px] text-justify">${z.description}</h5>
+            </div>
+            <div class="flex justify-between py-2">
+                <div class="text-center bg-[#DCFCE7] rounded-2xl">
+                <h1 class="font-medium text-[#15803D] text-sm px-1 ">${z.category}</h1>
+        </div>
+            <h1>৳${z.price}</h1></div>
+            <div class="text-center bg-[#15803D] rounded-2xl">
+                <h1 onclick="addtocart('${z.name}',${z.price})" class="font-medium text-white text-[16px] py-1">Add to Cart</h1>
+        </div>
+    </div>`
+    // console.log(treeinfo)
+})
+document.getElementById("trees").innerHTML+=treeinfo
+}
+function displaytree(z)
+{
+console.log(z)
+let url=`https://openapi.programming-hero.com/api/category/${z}`
+console.log(url)
+fetch(url)
+.then(x=>x.json())
+.then(y=>{
+    disableactive()
+    document.getElementById(`cat_${z}`).classList.add("active")
+    displayplantinfo(y.plants)})
+}
+const displaycat=(x)=>
+{
+    let category=``;
+    x.forEach(element => {
+        category+=`<div onclick="displaytree(${element.id})">
+            <h1 id="cat_${element.id}" class="btns p-2 text-base text-center sm:text-left hover:bg-slate-300 rounded">${element.category_name}</h1>
+        </div>`
+    });
+    document.getElementById("Categories").innerHTML+=category
+}
+function categories(){
+fetch("https://openapi.programming-hero.com/api/categories")
+.then(x=>x.json())
+.then(y=>displaycat(y.categories))
+}
+categories()
